74ba58a782c79b07e44abf68233f556c17a4f607
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2020-08-19 13:04:04 +0530

    Added yaml configuration

--- src/main/java/com/bank/csm/constants/BuildConstants.java
+++ src/main/java/com/bank/csm/constants/BuildConstants.java
@@ -9,6 +9,6 @@ public class BuildConstants {
 	
 	public static final String VERSION = "0.1";
 
-	public static final String BUILD = "2020_8_2_13_47_40-1";
+	public static final String BUILD = "2020_8_9_18_17_46-1";
 	
 }
